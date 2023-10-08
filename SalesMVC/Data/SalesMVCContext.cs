3cf373ff5a8b54bc3a0e41cc798366094f6c178d
filipemonteiro73@gmail.com <filipemonteiro73@gmail.com>
2023-10-08 17:42:09 -0300

    Servico de populacao do banco de dados

--- SalesMVC/Data/SalesMVCContext.cs
+++ SalesMVC/Data/SalesMVCContext.cs
@@ -18,5 +18,6 @@ namespace SalesMVC.Data
         public DbSet<Department> Department { get; set; }
         public DbSet<Seller> Seller { get; set; }
         public DbSet<SalesRecord> SalesRecords { get; set; }
+        public object SalesRecord { get; internal set; }
     }
 }
--- SalesMVC/Models/SalesRecord.cs
+++ SalesMVC/Models/SalesRecord.cs
@@ -12,18 +12,18 @@ namespace SalesMVC.Models
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public double Amount { get; set; }
-        public SaleStatus status { get; set; }
+        public SaleStatus Status { get; set; }
 
         public Seller Seller { get; set; }
 
-        public SalesRecord() { }
+        public SalesRecord() {}
 
         public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
         {
             Id = id;
             Date = date;
             Amount = amount;
-            this.status = status;
+            Status = status;
             Seller = seller;
         }
     }
