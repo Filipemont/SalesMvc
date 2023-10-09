9681138da26a5c152d86f23e8b170da7d64bb4cb
filipemonteiro73@gmail.com <filipemonteiro73@gmail.com>
2023-10-08 23:09:43 -0300

    Tratamento de exceção na deleção

--- SalesMVC/Controllers/SellersController.cs
+++ SalesMVC/Controllers/SellersController.cs
@@ -46,7 +46,7 @@ namespace SalesMVC.Controllers
                 var departments = await _departmentService.FindAllAsync();
                 var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
                 return View(viewModel);
-                
+
             }
             await _sellerService.InsertAsync(seller);
             return RedirectToAction(nameof(Index));
@@ -72,9 +72,16 @@ namespace SalesMVC.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
-        }
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
+            }
 
         public async Task<IActionResult> Details(int? id)
         {
@@ -117,7 +124,7 @@ namespace SalesMVC.Controllers
             if (!ModelState.IsValid)
             {
                 var departments = await _departmentService.FindAllAsync();
-                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments }; 
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
                 return View(viewModel);
             }
             if (id != seller.Id)
